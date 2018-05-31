472963f46556797bcb8979b161797eb995bb694f
Philip Heron <phil@sanslogic.co.uk>
2018-05-31 09:24:40 +0100

    Add missing license header

--- teletext.c
+++ teletext.c
@@ -1,3 +1,34 @@
+/* hacktv - Analogue video transmitter for the HackRF                    */
+/*=======================================================================*/
+/* Copyright 2018 Philip Heron <phil@sanslogic.co.uk>                    */
+/*                                                                       */
+/* This program is free software: you can redistribute it and/or modify  */
+/* it under the terms of the GNU General Public License as published by  */
+/* the Free Software Foundation, either version 3 of the License, or     */
+/* (at your option) any later version.                                   */
+/*                                                                       */
+/* This program is distributed in the hope that it will be useful,       */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of        */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         */
+/* GNU General Public License for more details.                          */
+/*                                                                       */
+/* You should have received a copy of the GNU General Public License     */
+/* along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+/* -=== Teletext encoder ===-
+ * 
+ * Encodes a teletext stream, inserting packets into the VBI area of the
+ * video signal. Teletext pages in the TTI file format are supported.
+ * 
+ * This version only works with 625-line PAL modes. There are NTSC and
+ * SECAM variations of Teletext but those are not currently supported.
+ * 
+ * TODO: The clock on the header line will not be accurate when not
+ * transmitting live. This could be fixed by calculating the time based
+ * on the timecode, but it will need some flag to indicate if the output
+ * is live or not.
+ * 
+*/
 
 #include <stdio.h>
 #include <stdint.h>
