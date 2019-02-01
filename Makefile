a97cd254e693f16263bae4672945bcc9c3352c87
Philip Heron <phil@sanslogic.co.uk>
2019-02-01 19:18:54 +0000

    Updates to the Windows build script

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 
 CC      := $(CROSS_HOST)gcc
 PKGCONF := $(CROSS_HOST)pkg-config
-CFLAGS  := -g -Wall -pthread -O3
-LDFLAGS := -g -lm -pthread
+CFLAGS  := -g -Wall -pthread -O3 $(EXTRA_CFLAGS)
+LDFLAGS := -g -lm -pthread $(EXTRA_LDFLAGS)
 OBJS    := hacktv.o common.o fir.o vbidata.o teletext.o wss.o video.o videocrypt.o syster.o nicam728.o test.o ffmpeg.o file.o hackrf.o
-PKGS    := libavcodec libavformat libavdevice libswscale libswresample libavutil libhackrf
+PKGS    := libavcodec libavformat libavdevice libswscale libswresample libavutil libhackrf $(EXTRA_PKGS)
 
 SOAPYSDR := $(shell $(PKGCONF) --exists SoapySDR && echo SoapySDR)
 ifeq ($(SOAPYSDR),SoapySDR)
--- build_win64.sh
+++ build_win64.sh
@@ -1,56 +1,82 @@
 #!/bin/bash
 
 set -e
+set -x
 
 HOST=x86_64-w64-mingw32
 PREFIX=$(pwd)/build_win64/install_root
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
-rm -rf build_win64
 mkdir -p $PREFIX
 cd build_win64
 
-git clone https://github.com/libusb/libusb.git
-cd libusb
-./bootstrap.sh
-./configure --host=$HOST --prefix=$PREFIX --enable-static --disable-shared
-make -j4 install
-cd ..
+if [[ ! -f $PREFIX/lib/libusb-1.0.a ]]; then
+	
+	if [[ ! -f libusb-1.0.22.tar.bz2 ]]; then
+		wget https://github.com/libusb/libusb/releases/download/v1.0.22/libusb-1.0.22.tar.bz2
+		tar -xvjf libusb-1.0.22.tar.bz2
+	fi
+	
+	cd libusb-1.0.22
+	./configure --host=$HOST --prefix=$PREFIX --enable-static --disable-shared
+	make -j4 install
+	cd ..
+fi
 
-wget http://www.fftw.org/fftw-3.3.8.tar.gz
-tar -xvzf fftw-3.3.8.tar.gz
-cd fftw-3.3.8
-./configure --host=$HOST --prefix=$PREFIX --enable-static --disable-shared --disable-dependency-tracking --disable-threads
-make -j4 install
-cd ..
+if [[ ! -f $PREFIX/lib/libhackrf.a ]]; then
+	
+	if [[ ! -f hackrf-2018.01.1.tar.gz ]]; then
+		wget https://github.com/mossmann/hackrf/archive/v2018.01.1/hackrf-2018.01.1.tar.gz
+		tar -xvzf hackrf-2018.01.1.tar.gz
+	fi
+	
+	rm -rf hackrf-2018.01.1/host/libhackrf/build
+	mkdir -p hackrf-2018.01.1/host/libhackrf/build
+	cd hackrf-2018.01.1/host/libhackrf/build
+	mingw64-cmake \
+		-DCMAKE_INSTALL_PREFIX=$PREFIX \
+		-DCMAKE_INSTALL_LIBPREFIX=$PREFIX/lib \
+		-DLIBUSB_INCLUDE_DIR=$PREFIX/include/libusb-1.0 \
+		-DLIBUSB_LIBRARIES=$PREFIX/lib/libusb-1.0.a
+	make -j4 install
+	cd ../../../..
+	mv $PREFIX/bin/*.a $PREFIX/lib/
+	find $PREFIX -name libhackrf\*.dll\* -delete
+fi
 
-git clone https://github.com/mossmann/hackrf.git
-cd hackrf/host
-mingw64-cmake -DLIBUSB_INCLUDE_DIR=$PREFIX/include/libusb-1.0 -DFFTW_LIBRARIES=$PREFIX/lib -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
-make -j4 install
-# The libs end up in /bin - can I fix this above?
-mv $PREFIX/bin/libhackrf.*.a $PREFIX/lib/
-rm $PREFIX/bin/libhackrf.dll
-cd ../..
+if [[ ! -f $PREFIX/lib/libfdk-aac.a ]]; then
+	
+	if [[ ! -f fdk-aac ]]; then
+		git clone https://github.com/mstorsjo/fdk-aac.git
+	fi
+	
+	cd fdk-aac
+	./autogen.sh
+	./configure --host=$HOST --prefix=$PREFIX --enable-static --disable-shared
+	make -j4 install
+	cd ..
+fi
 
-git clone https://github.com/mstorsjo/fdk-aac.git
-cd fdk-aac
-./autogen.sh
-./configure --host=$HOST --prefix=$PREFIX --enable-static --disable-shared
-make -j4 install
-cd ..
+if [[ ! -f $PREFIX/lib/libavformat.a ]]; then
+	
+	if [[ ! -f ffmpeg ]]; then
+		git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
+	fi
+	
+	cd ffmpeg
+	./configure \
+		--enable-gpl --enable-nonfree --enable-libfdk-aac \
+		--enable-static --disable-shared --disable-programs \
+		--disable-outdevs --disable-encoders \
+		--arch=x86_64 --target-os=mingw64 --cross-prefix=$HOST- \
+		--pkg-config=pkg-config --prefix=$PREFIX
+	make -j4 install
+	cd ..
+fi
 
-git clone https://github.com/FFmpeg/FFmpeg.git ffmpeg
-cd ffmpeg
-PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig ./configure \
-	--enable-gpl --enable-nonfree --enable-libfdk-aac \
-	--disable-outdevs --disable-encoders \
-	--arch=x86_64 --target-os=mingw64 --cross-prefix=$HOST- \
-	--pkg-config=pkg-config --prefix=$PREFIX
-make -j4 install
 cd ..
-
-cd ..
-CROSS_HOST=$HOST- PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig make -j4
+CROSS_HOST=$HOST- make -j4 EXTRA_LDFLAGS="-static" EXTRA_PKGS="libusb-1.0"
+mv hacktv hacktv.exe
 $HOST-strip hacktv.exe
 
 echo "Done"
