fc519b017e0e5ea336e98a10c8c0f460908e754d
Philip Heron <phil@sanslogic.co.uk>
2019-12-10 18:59:23 +0000

    Slight adjustment to the Syster VBI timing

--- syster.c
+++ syster.c
@@ -573,7 +573,7 @@ void ng_render_line(ng_t *s)
 		}
 		
 		/* Render the line */
-		vbidata_render_nrz(s->lut, s->vbi[s->vbi_seq++], -48, NG_VBI_BYTES * 8, VBIDATA_LSB_FIRST, s->vid->output, 2);
+		vbidata_render_nrz(s->lut, s->vbi[s->vbi_seq++], -45, NG_VBI_BYTES * 8, VBIDATA_LSB_FIRST, s->vid->output, 2);
 		
 		if(s->vbi_seq == 10)
 		{
