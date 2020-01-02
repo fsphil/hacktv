d2cb0bd34d2126715cdd60a036245eccd09f1fdf
Philip Heron <phil@sanslogic.co.uk>
2020-01-02 09:22:33 +0000

    Add NTSC and SECAM FM modes

--- video.c
+++ video.c
@@ -350,6 +350,56 @@ const vid_config_t vid_config_secam_dk = {
 	.fm_audio_deviation = 50000, /* +/- Hz */
 };
 
+const vid_config_t vid_config_secam_fm = {
+	
+	/* SECAM FM (satellite) */
+	.output_type    = HACKTV_INT16_COMPLEX,
+	
+	.modulation     = VID_FM,
+	.fm_level       = 1.0,
+	.fm_deviation   = 10000000, /* Hz */
+	
+	.level          = 1.0, /* Overall signal level */
+	
+	.video_level    = 1.00, /* Power level of video */
+	.fm_audio_level = 0.05, /* FM audio carrier power level */
+	
+	.type           = VID_RASTER_625,
+	.frame_rate_num = 25,
+	.frame_rate_den = 1,
+	.lines          = 625,
+	.active_lines   = 576,
+	.active_width   = 0.00005195, /* 51.95µs */
+	.active_left    = 0.00001040, /* |-->| 10.40µs */
+	
+	.hsync_width       = 0.00000470, /* 4.70 ±0.20µs */
+	.vsync_short_width = 0.00000235, /* 2.35 ±0.10µs */
+	.vsync_long_width  = 0.00002730, /* 2.73 ±0.20µs */
+	
+	.white_level    =  0.50,
+	.black_level    = -0.20,
+	.blanking_level = -0.20,
+	.sync_level     = -0.50,
+	
+	.colour_mode    = VID_SECAM,
+	.burst_left     = 0.00000560, /* |-->| 5.6 ±0.1µs */
+	
+	.gamma          = 1.2,
+	.rw_co          = 0.299, /* R weight */
+	.gw_co          = 0.587, /* G weight */
+	.bw_co          = 0.114, /* B weight */
+	.iu_co          = 1.000,
+	.iv_co          = 0.000,
+	.qu_co          = 0.000,
+	.qv_co          = -1.000,
+	
+	.fm_mono_carrier    = 6500000, /* Hz */
+	//.fm_left_carrier    = 7200000, /* Hz */
+	//.fm_right_carrier   = 7020000, /* Hz */
+	.fm_audio_preemph   = 0.000050, /* Seconds */
+	.fm_audio_deviation = 85000, /* +/- Hz */
+};
+
 const vid_config_t vid_config_secam = {
 	
 	/* Composite SECAM */
@@ -439,6 +489,61 @@ const vid_config_t vid_config_ntsc_m = {
 	.fm_mono_carrier    = 4500000, /* Hz */
 	.fm_audio_preemph   = 0.000075, /* Seconds */
 	.fm_audio_deviation = 25000, /* +/- Hz */
+};
+
+const vid_config_t vid_config_ntsc_fm = {
+	
+	/* NTSC FM (satellite) */
+	.output_type    = HACKTV_INT16_COMPLEX,
+	
+	.modulation     = VID_FM,
+	.fm_level       = 1.0,
+	.fm_deviation   = 10000000, /* Hz */
+	
+	.level          = 1.0, /* Overall signal level */
+	
+	.video_level    = 1.00, /* Power level of video */
+	.fm_audio_level = 0.05, /* FM audio carrier power level */
+	
+	.type           = VID_RASTER_625,
+	.frame_rate_num = 25,
+	.frame_rate_den = 1,
+	.lines          = 625,
+	.active_lines   = 576,
+	.active_width   = 0.00005195, /* 51.95µs */
+	.active_left    = 0.00001040, /* |-->| 10.40µs */
+	
+	.hsync_width       = 0.00000470, /* 4.70 ±0.20µs */
+	.vsync_short_width = 0.00000235, /* 2.35 ±0.10µs */
+	.vsync_long_width  = 0.00002730, /* 2.73 ±0.20µs */
+	
+	.white_level    =  0.50,
+	.black_level    = -0.20,
+	.blanking_level = -0.20,
+	.sync_level     = -0.50,
+	
+	.colour_mode    = VID_NTSC,
+	.burst_width    = 0.00000250, /* 2.5 ±0.28µs */
+	.burst_left     = 0.00000530, /* |-->| 5.3 ±0.1µs */
+	.burst_level    = 4.0 / 10.0, /* 4/10 of white - blanking level */
+	.colour_carrier = 5000000.0 * 63 / 88,
+	.colour_lookup_lines = 2, /* The carrier repeats after 2 lines */
+	
+	.gamma          = 1.2,
+	
+	.rw_co          =  0.299, /* R weight */
+	.gw_co          =  0.587, /* G weight */
+	.bw_co          =  0.114, /* B weight */
+	.iu_co          = -0.177,
+	.iv_co          =  0.768,
+	.qu_co          =  0.626,
+	.qv_co          = -0.082,
+	
+	.fm_mono_carrier    = 6500000, /* Hz */
+	//.fm_left_carrier    = 7200000, /* Hz */
+	//.fm_right_carrier   = 7020000, /* Hz */
+	.fm_audio_preemph   = 0.000050, /* Seconds */
+	.fm_audio_deviation = 85000, /* +/- Hz */
 };
 
 const vid_config_t vid_config_ntsc = {
@@ -1139,8 +1244,10 @@ const vid_configs_t vid_configs[] = {
 	{ "l",             &vid_config_secam_l          },
 	{ "d",             &vid_config_secam_dk         },
 	{ "k",             &vid_config_secam_dk         },
+	{ "secam-fm",      &vid_config_secam_fm         },
 	{ "secam",         &vid_config_secam            },
 	{ "m",             &vid_config_ntsc_m           },
+	{ "ntsc-fm",       &vid_config_ntsc_fm          },
 	{ "ntsc",          &vid_config_ntsc             },
 	{ "d2mac-am",      &vid_config_d2mac_am         },
 	{ "d2mac-fm",      &vid_config_d2mac_fm         },
