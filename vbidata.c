6174d0a92e29c10fbc7d2058a0246ddc1f853043
Alex James <captainjack@clara.co.uk>
2021-02-03 20:33:11 +0000

    Update VBI RC to RRC

--- vbidata.c
+++ vbidata.c
@@ -29,7 +29,7 @@ static double _sinc(double x)
 static double _raised_cosine(double x, double b, double t)
 {
 	if(x == 0) return(1.0);
-        return(_sinc(x / t) * (cos(M_PI * b * x / t) / (1.0 - (4.0 * b * b * x * x / (t * t)))));
+        return sqrt(_sinc(x / t) * (cos(M_PI * b * x / t) / (1.0 - (4.0 * b * b * x * x / (t * t)))));
 }
 
 static size_t _vbidata_init(int16_t *lut, unsigned int swidth, unsigned int dwidth, int16_t level, int filter, double beta)
