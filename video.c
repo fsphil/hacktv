7d623ed2560cca6642a45659c86fd763150bd653
David Bullado <david.bullado@gmail.com>
2022-02-28 23:53:56 +0100

    update am audio level for secam L

--- video.c
+++ video.c
@@ -384,7 +384,7 @@ const vid_config_t vid_config_secam_l = {
 	.level          = 1.0, /* Overall signal level */
 	
 	.video_level    = 0.80, /* Power level of video */
-	.am_audio_level = 0.10, /* AM audio carrier power level */
+	.am_audio_level = 0.20, /* AM audio carrier power level */
 	.nicam_level    = 0.04, /* NICAM audio carrier power level */
 	
 	.type           = VID_RASTER_625,
