d617b4b11da1d0e022b83b4ee67776c6cc199c83
Philip Heron <phil@sanslogic.co.uk>
2023-01-14 20:23:27 +0000

    Use int in place of int16_t for vbi data level

--- vbidata.h
+++ vbidata.h
@@ -23,7 +23,7 @@
 #define VBIDATA_LSB_FIRST (0)
 #define VBIDATA_MSB_FIRST (1)
 
-extern int16_t *vbidata_init(unsigned int swidth, unsigned int dwidth, int16_t level, int filter, double beta);
+extern int16_t *vbidata_init(unsigned int swidth, unsigned int dwidth, int level, int filter, double beta);
 extern int16_t *vbidata_init_step(unsigned int swidth, unsigned int dwidth, int level, double rise);
 extern void     vbidata_render_nrz(const int16_t *lut, const uint8_t *src, int offset, size_t length, int order, int16_t *dst, size_t step);
 
